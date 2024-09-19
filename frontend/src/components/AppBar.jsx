cb6f3fa014a1cc2de9626745ec6276208834b99f
Sahil Shirsekar <120778958+sahilshirsekar@users.noreply.github.com>
2024-09-19 12:14:04 +0530

    Update AppBar.jsx

--- frontend/src/components/AppBar.jsx
+++ frontend/src/components/AppBar.jsx
@@ -2,7 +2,7 @@ import Heading from "./Heading"
 function AppBar(){
     return (
         <div className="flex justify-between">
-        <Heading label={"PaytmApp"}/>
+        <Heading label={"PayMate"}/>
         <h1 className="text-black text-base text-center m-3  font-semibold ">Hello , user</h1>
         </div>
     )
